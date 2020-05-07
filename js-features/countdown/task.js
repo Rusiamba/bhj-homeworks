0a6150b64ad3e858275314a69dd4453a3dbc3a0b
Руслан <42304481+Rusiamba@users.noreply.github.com>
2020-05-07 21:07:53 +0300

    Update task.js

---
+++ js-features/countdown/task.js
@@ -0,0 +1,14 @@
+let i = document.getElementById('timer')
+let x = 59;
+countdown();
+function countdown(){  
+    document.getElementById('timer').innerHTML = x;
+    x--; 
+    if (x<0){
+        clearTimeout()
+        alert("Вы победили в конкурсе!");
+    }
+    else {
+        timer = setTimeout(countdown, 1000);
+    }
+}
