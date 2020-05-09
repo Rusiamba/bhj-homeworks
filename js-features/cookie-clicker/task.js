321def11d8a178a26730d42d749a9291ccb3a9f3
Руслан <42304481+Rusiamba@users.noreply.github.com>
2020-05-09 10:02:22 +0300

    Update task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -6,6 +6,10 @@ y.count = 0;
 
 x.addEventListener("click", function() {
     y.count++;
-    x.width = 250;
+    if (x.width === 250) {
+        x.width = 200;
+    } else if (x.width === 200) {
+        x.width = 250;
+    }
     clicker__counter.innerHTML = y.count;
 });
