a1ff67c2bba4facb81a2be047a9c56d040779b01
Руслан <42304481+Rusiamba@users.noreply.github.com>
2020-05-19 16:17:32 +0300

    Update task.js

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,9 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+   document.addEventListener('keypress', (event) => {
+      this.currentSymbol.textContent === event.key.toLowerCase() ? this.success() : this.fail();
+    })
   }
 
   success() {
