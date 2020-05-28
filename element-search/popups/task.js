2e91cab9aa60f54c7a88be32b261e94d57764cb5
Руслан <42304481+Rusiamba@users.noreply.github.com>
2020-05-28 11:13:26 +0300

    Update task.js

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,18 +1,21 @@
-let modmain = document.getElementById('modal_main');
-let modsuccess = document.getElementById('modal_success');
-let clouse = document.querySelector('.modal__close');
+const popUp = setTimeout(() => {    
+    let pop1 = document.getElementById("modal_main");
+    let pop2 = document.getElementById("modal_success");
+    pop1.className = "modal modal_active";
+    
+    pop1.getElementsByClassName("modal__close").item(0).onclick = () => {  
+        pop1.className = "modal";
+    }
+    pop1.getElementsByClassName("modal__close").item(1).onclick = () => { 
+        pop1.className = "modal";
+        pop2.className = "modal modal_active";
+    }
+    pop2.getElementsByClassName("modal__close").item(0).onclick = () => {
+        pop2.className = "modal";
+    }
+    pop2.getElementsByClassName("btn btn_success").item(0).onclick = () => {
+        pop2.className = "modal";        
+    }   
+}, 2000);
 
-
-clouse.addEventListener('click', function(){
-    modmain.style.display = "none";
-    modsuccess.style.display = "none";
-});
-
-modmain.onclick = function() {
-    modsuccess.style.display ="block";
-    modmain.style.display = "none";
-}
-
-modsuccess.onclick = function() {
-    modsuccess.style.display = "none";
-}
+popUp;
