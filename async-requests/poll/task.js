b128cf5784b9b13a61939bdbc3407edf8482587b
Руслан <42304481+Rusiamba@users.noreply.github.com>
2020-06-01 07:31:37 +0300

    Update task.js

---
+++ async-requests/poll/task.js
@@ -0,0 +1,24 @@
+let pt = document.getElementById('poll__title');
+let pa = document.getElementById('poll__answers');
+let str = '';
+let xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php')
+xhr.addEventListener('readystatechange', function () {
+    if (this.readyState == xhr.DONE && this.status == 200) {
+        let txt = JSON.parse(this.responseText);
+        pt.insertAdjacentText("afterbegin", txt.data.title);
+        for (let i = 0; i < txt.data.answers.length; i++) {
+            str += `<button class='poll__answer'>${txt.data.answers[i]}</btn>`
+        }
+        pa.insertAdjacentHTML("afterbegin", str);
+    }
+
+    let buttnAns = document.querySelectorAll('.poll__answer')
+    buttnAns.forEach(arr => {
+        arr.onclick= function () {
+            alert('Спасибо, ваш голос засчитан!')
+        }
+    });
+});
+xhr.send();
